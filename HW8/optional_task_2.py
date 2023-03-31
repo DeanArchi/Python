fc3a1139e8cba18d7a8a89057d58bdd9cb1fc0ed
Dean Archi <ivan.artsibarskiy@gmail.com>
2023-03-31 14:21:43 +0300

    some changes in optional_task_2.py

--- HW8/optional_task_2.py
+++ HW8/optional_task_2.py
@@ -1,9 +1,10 @@
 # 1 option
 def check(check_lst):
-    if type(check_lst) is int:
-        return True
-    else:
-        return False
+    # if type(check_lst) is int:
+    #     return True
+    # else:
+    #     return False
+    return isinstance(check_lst, int)
 
 
 lst = [1, 2, 3, "1", "2", "3", "Hello", 5, "(1, 2, 3)", 15, 0, "10", "11", "12", "asd"]
@@ -15,10 +16,11 @@ print(result)
 
 
 def another_check(check_lst):
-    if check_lst.isdigit():
-        return True
-    else:
-        return False
+    # if check_lst.isdigit():
+    #     return True
+    # else:
+    #     return False
+    return check_lst.isdigit()
 
 
 lst1 = ["1", "2", "dfg24", "Hello", "22", "World", "5432", "qwerty12345"]
