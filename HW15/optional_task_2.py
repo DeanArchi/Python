6a471bce6d90079b9c2e25784e9dd3ffad7beb83
Dean Archi <ivan.artsibarskiy@gmail.com>
2023-04-28 14:14:56 +0300

    HW15 is done

---
+++ HW15/optional_task_2.py
@@ -0,0 +1,15 @@
+import re
+
+filename = input("Input filename: ")
+
+try:
+    with open(filename, "r") as file:
+        content = file.read()
+except FileNotFoundError:
+    print(f"Unknown file with name {filename}")
+else:
+    change_pattern = re.compile(r"(?<=\S)\S+@\S+\.\S+(?=\S)")
+    changed_content = re.sub(change_pattern, "***@***", content)
+    with open(filename, "a") as file:
+        file.write("\n\nChanged text:\n")
+        file.write(changed_content)
