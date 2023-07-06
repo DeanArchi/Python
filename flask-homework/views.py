5e305ff93041fc12d662632ae42c71beb920aa02
Dean Archi <ivan.artsibarskiy@gmail.com>
2023-07-06 15:18:05 +0300

    .

--- flask-homework/views.py
+++ flask-homework/views.py
@@ -246,10 +246,10 @@ def login():
         <form method="POST" action="/login">
             <label for="username">Username</label>
             <input type="text" name="username" id="username"><br><br>
-        
+
             <label for="password">Password</label>
             <input type="password" name="password" id="password"><br><br>
-        
+
             <input type="submit" value="Submit">
         </form>
         """
