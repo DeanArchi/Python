a30148194a1cf1d1beae64545f37ad6b787c635f
Dean Archi <ivan.artsibarskiy@gmail.com>
2023-07-11 12:29:03 +0300

    hw35 is done

---
+++ flask-homework/models.py
@@ -0,0 +1,22 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    price = db.Column(db.Integer)
+
+
+class Purchase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User")
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"))
+    book = db.relationship("Book")
