b67da9f44dad4f7d5d82b68bcd34e72f31fbf09f
Dean Archi <ivan.artsibarskiy@gmail.com>
2023-07-20 13:40:49 +0300

    HW38 is done

--- django_project/books/models.py
+++ django_project/books/models.py
@@ -6,6 +6,9 @@ class Book(models.Model):
     author = models.CharField(max_length=255, null=False)
     price = models.IntegerField(null=False)
 
+    def __str__(self):
+        return f'book {self.id}, title: {self.title}, author: {self.author}'
+
     class Meta:
         db_table = 'book'
         unique_together = ('title', 'author')
--- django_project/purchases/admin.py
+++ django_project/purchases/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from purchases.models import Purchase
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_user_id', 'get_book_id', 'purchase_date')
+
+    def get_user_id(self, obj):
+        if obj.user:
+            return f'{obj.user.last_name} {obj.user.first_name}'
+        else:
+            return None
+
+    get_user_id.short_description = 'user'
+
+    def get_book_id(self, obj):
+        if obj.book:
+            return obj.book.title
+        else:
+            return None
+
+    get_book_id.short_description = 'book'
