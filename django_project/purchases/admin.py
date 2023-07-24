5b941f6e89a0af6ceeecbd634b9b86ba872b902d
Dean Archi <ivan.artsibarskiy@gmail.com>
2023-07-24 13:28:22 +0300

    some changes in purchases/admin and users/admin

--- django_project/purchases/admin.py
+++ django_project/purchases/admin.py
@@ -5,20 +5,20 @@ from purchases.models import Purchase
 
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_user_id', 'get_book_id', 'purchase_date')
+    list_display = ('id', 'user', 'book', 'purchase_date')
 
-    def get_user_id(self, obj):
+    def user(self, obj):
         if obj.user:
             return f'{obj.user.last_name} {obj.user.first_name}'
         else:
             return None
 
-    get_user_id.short_description = 'user'
+    user.short_description = 'user'
 
-    def get_book_id(self, obj):
+    def book(self, obj):
         if obj.book:
             return obj.book.title
         else:
             return None
 
-    get_book_id.short_description = 'book'
+    book.short_description = 'book'
--- django_project/users/admin.py
+++ django_project/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 
 @admin.register(get_user_model())
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'first_name', 'last_name', 'age')
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'age', 'is_staff')
